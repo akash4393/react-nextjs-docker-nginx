ac5ab12968bffdeef74a408c110368fe5160486a
Akash Singh <akash4393@gmail.com>
2021-08-16 23:34:03 +0530

    Added eslint, prettier & husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 
 export const SOMETHING = 'something'
 SOMETHING = 'somethingelse 2'
+let somethingelse = 'test'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
