653fe3841b709304f3dc8d63187b885de5a2ce6b
Akash Singh <akash4393@gmail.com>
2021-08-22 16:32:59 +0530

    Added eslint-webpack-plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
+/* eslint-disable */
+const ESLintPlugin = require('eslint-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      // Add this plugin only in dev mode
+      config.plugins.push(
+        new ESLintPlugin({
+          context: './', // Location where it will scan all the files
+          extensions: ['js', 'jsx', 'ts', 'tsx'], // File formats that should be scanned
+          exclude: ['node_modules', 'dist'], // Exclude everything in these folders
+        })
+      )
+    }
+    return config
+  },
 }
